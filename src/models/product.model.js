e96567cacf70f950d7d1e6ae9a691fc12b4857f8
Vu Xuan Sang <sangvuxuan6784@gmail.com>
2025-09-18 01:09:11 +0700

    createProduc~err

--- src/models/product.model.js
+++ src/models/product.model.js
@@ -1,23 +1,43 @@
 const {model, Schema} = require('mongoose'); // Erase if already required
 
-const DOCUMENT_NAME = "Apikey"
-const COLLECTION_NAME = "Apikeys"
+const DOCUMENT_NAME = "Product"
+const COLLECTION_NAME = "Products"
 
 const productSchema = new Schema({
     product_name: {type: String, required: true},
-
+    product_thumb: {type: String, required: true},
+    product_description: String,
+    product_price:{type: Number, required:true},
+    product_quantity:{type:Number, required:true},
+    product_type:{type:String, required:true, enum:['Electronics','Clothings','Furniture']},
+    product_shop: String,
+    product_attributes:{type: Schema.Types.Mixed, required:true }
+},{
+    collection:COLLECTION_NAME,
+    timestamps:true
 })
 
+//define products type = clothing
 const clothingSchema = new Schema({
-    brand: {type:String, require: true},
+    brand: {type: String, require: true},
     size: String,
-    material:String
+    material: String
+},{
+    collection:'clothes',
+    timestamps: true
+})
+
+const electronicSchema = new Schema({
+    manufacturer: {type: String, require: true},
+    model: String,
+    color: String
 },{
     collection:'electronics',
-    timestamps:true
+    timestamps: true
 })
 
 module.exports ={
     product: model(DOCUMENT_NAME, productSchema),
-    electronic: model('Elec')
+    clothing: model('Clothing', clothingSchema),
+    electronic: model('Electronics', electronicSchema)
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -8,5 +8,6 @@ const {apiKey} = require('../auth/checkAuth')
 router.use(apiKey)
 
 router.use('/v1/api', require('./access'))
+router.use('/v1/api/product', require('./product'))
 
 module.exports = router;
